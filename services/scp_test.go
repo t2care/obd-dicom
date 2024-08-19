a06b40cc5984d26f3b2b90a1b3b359f08ebdf405
t2nt <106388825+t2nt@users.noreply.github.com>
2024-08-19 11:10:42 +0200

    Add OnAssociationRelease and AssociationID (#6)
    * Add ID to AAssociationRQ
    
    * Add OnAssociationRelease
    
    * Update readme
    
    * Update test
    
    * Add more time for server to close asso

---
+++ services/scp_test.go
@@ -0,0 +1,56 @@
+package services
+
+import (
+	"testing"
+	"time"
+
+	"github.com/one-byte-data/obd-dicom/network"
+)
+
+func Test_Association_ID(t *testing.T) {
+	_, testSCP := StartSCP(t, 1043)
+	var onAssociationRequestID int64
+	var onAssociationReleaseID int64
+	testSCP.OnAssociationRequest(func(request network.AAssociationRQ) bool {
+		onAssociationRequestID = request.GetID()
+		return true
+	})
+	testSCP.OnAssociationRelease(func(request network.AAssociationRQ) {
+		onAssociationReleaseID = request.GetID()
+	})
+	tests := []struct {
+		name    string
+		wantErr bool
+	}{
+		{
+			name:    "Asso 1",
+			wantErr: false,
+		},
+		{
+			name:    "Asso 2",
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			d := NewSCU(&network.Destination{
+				Name:      "Test Destination",
+				CalledAE:  "TEST_SCP",
+				CallingAE: "TEST_SCU",
+				HostName:  "localhost",
+				Port:      1043,
+				IsCFind:   false,
+				IsCMove:   false,
+				IsCStore:  false,
+				IsTLS:     false,
+			})
+			if err := d.EchoSCU(0); (err != nil) != tt.wantErr {
+				t.Errorf("scu.EchoSCU() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			time.Sleep(100 * time.Millisecond) // wait for association closed
+			if onAssociationRequestID != onAssociationReleaseID {
+				t.Errorf("onAssociationRequestID = %v, onAssociationReleaseID = %v", onAssociationRequestID, onAssociationReleaseID)
+			}
+		})
+	}
+}
