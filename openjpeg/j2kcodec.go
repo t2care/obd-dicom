eae0b9af4d74daa2de7508ea5ce6a7eb665102a4
Tri <tri.nguyentai@t2technology.fr>
2024-08-19 11:47:35 +0200

    Update syntax

--- openjpeg/j2kcodec.go
+++ openjpeg/j2kcodec.go
@@ -5,6 +5,6 @@ package openjpeg
 import "github.com/one-byte-data/obd-dicom/dictionary/transfersyntax"
 
 func init() {
-	transfersyntax.Register(transfersyntax.JPEG2000Lossless, J2Kdecode, J2Kencode)
-	transfersyntax.Register(transfersyntax.JPEG2000, J2Kdecode, J2Kencode)
+	transfersyntax.RegisterCodec(transfersyntax.JPEG2000Lossless.UID, J2Kdecode, J2Kencode)
+	transfersyntax.RegisterCodec(transfersyntax.JPEG2000.UID, J2Kdecode, J2Kencode)
 }
