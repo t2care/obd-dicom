909f9b092a22028c8f3787ec872c980dc42acd74
t2nt <106388825+t2nt@users.noreply.github.com>
2024-09-06 15:31:10 +0200

    Add Dicom Mapper to structure (#24)
    * Add mapper
    
    * Add keywork option

---
+++ utils/mapper.go
@@ -0,0 +1,93 @@
+package utils
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+
+	"github.com/t2care/obd-dicom/dictionary/tags"
+	"github.com/t2care/obd-dicom/media"
+)
+
+var key string
+
+func MapDicomDataToStruct(dicomDataset *media.DcmObj, targetStruct any, keyword ...string) (err error) {
+	v := reflect.ValueOf(targetStruct)
+	t := reflect.TypeOf(targetStruct).Kind()
+	if t != reflect.Ptr {
+		return fmt.Errorf("targerStruct must be a pointer")
+	}
+	if len(key) > 0 {
+		key = keyword[0]
+	}
+	recursiveFill(dicomDataset, v.Elem())
+	return
+}
+
+// recursiveFill analyze recursively the target structure and find corresponding Dicom value in the dataset.
+func recursiveFill(dataset *media.DcmObj, targetStructure reflect.Value) {
+	if targetStructure.Kind() == reflect.Struct {
+		targetType := targetStructure.Type()
+		for i := 0; i < targetStructure.NumField(); i++ {
+			field := targetType.Field(i)
+			fieldName := field.Name
+			fieldType := field.Type
+			if key == "" {
+				key = "dicom"
+			}
+			groupElem := field.Tag.Get(key)
+			switch fieldType.Kind() {
+			case reflect.Struct:
+				recursiveFill(dataset, targetStructure.Field(i))
+			case reflect.Slice:
+				goDeeper(fieldType, dataset, targetStructure, fieldName, groupElem)
+			default:
+				fillElement(fieldType, dataset, targetStructure, fieldName, groupElem)
+			}
+
+		}
+	}
+}
+
+// goDeeper is called when a struct field is a Slice of another type, either a base type or a struct type
+func goDeeper(fieldType reflect.Type, dataset *media.DcmObj, targetStructure reflect.Value, fieldName, groupElem string) {
+	sliceElement := fieldType.Elem()
+	if sliceElement.Kind() == reflect.Struct {
+		s := reflect.New(sliceElement)
+		recursiveFill(dataset, s.Elem())
+		f := targetStructure.FieldByName(fieldName)
+		f = reflect.MakeSlice(f.Type(), 1, 1)
+		f.Index(0).Set(s.Elem())
+		targetStructure.FieldByName(fieldName).Set(f)
+	} else {
+		fillElement(fieldType, dataset, targetStructure, fieldName, groupElem)
+	}
+}
+
+func fillElement(fieldType reflect.Type, dataset *media.DcmObj, targetStructure reflect.Value, fieldName, groupElem string) {
+	var group, elem string
+	dicomFieldTags := strings.Split(groupElem, ",")
+	if len(dicomFieldTags) == 2 {
+		group = dicomFieldTags[0]
+		elem = dicomFieldTags[1]
+	}
+	if group == "" || elem == "" {
+		return
+	}
+	groupHex, err := strconv.ParseInt("0x"+group, 0, 16)
+	if err != nil {
+		return
+	}
+	elemHex, err := strconv.ParseInt("0x"+elem, 0, 16)
+	if err != nil {
+		return
+	}
+	tag := &tags.Tag{Group: uint16(groupHex), Element: uint16(elemHex)}
+	switch fieldType.Kind() {
+	case reflect.String:
+		targetStructure.FieldByName(fieldName).SetString(dataset.GetString(tag))
+	case reflect.Uint8, reflect.Uint16:
+		targetStructure.FieldByName(fieldName).SetUint(uint64(dataset.GetUShort(tag)))
+	}
+}
---
+++ utils/mapper_test.go
@@ -0,0 +1,73 @@
+package utils
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/t2care/obd-dicom/media"
+)
+
+func TestMapDicomDataToStruct(t *testing.T) {
+	obj, _ := media.NewDCMObjFromFile("../samples/test.dcm", &media.ParseOptions{SkipPixelData: true})
+	type instance struct {
+		BitsAllocated uint8 `dicom:"0028,0100"`
+	}
+	type series struct {
+		SeriesNumber string `dicom:"0020,0011"`
+		Instance     []instance
+	}
+	type study struct {
+		PatientName string `dicom:"0010,0010"`
+		Series      []series
+	}
+	type dicomweb struct {
+		PatientName string `json:"0010,0010"`
+	}
+	type args struct {
+		dicomDataset *media.DcmObj
+		targetStruct any
+	}
+	tests := []struct {
+		name       string
+		args       args
+		wantErr    bool
+		wantTarget any
+		keywork    string
+	}{
+		{
+			name:       "Parse error",
+			args:       args{obj, study{}},
+			wantErr:    true,
+			wantTarget: study{},
+		},
+		{
+			name:    "Parse structure",
+			args:    args{obj, &study{}},
+			wantErr: false,
+			wantTarget: &study{
+				PatientName: "ACR PHANTOM",
+				Series: []series{{
+					SeriesNumber: "301",
+					Instance:     []instance{{BitsAllocated: 16}},
+				}},
+			},
+		},
+		{
+			name:       "Parse with other keywork",
+			args:       args{obj, &dicomweb{}},
+			keywork:    "json",
+			wantErr:    false,
+			wantTarget: &dicomweb{PatientName: "ACR PHANTOM"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := MapDicomDataToStruct(tt.args.dicomDataset, tt.args.targetStruct, tt.keywork); (err != nil) != tt.wantErr {
+				t.Errorf("MapDicomDataToStruct() error = %v, wantErr %v", err, tt.wantErr)
+			}
+			if !reflect.DeepEqual(tt.args.targetStruct, tt.wantTarget) {
+				t.Errorf("MapDicomDataToStruct() = %v, want %v", tt.args.targetStruct, tt.wantTarget)
+			}
+		})
+	}
+}
