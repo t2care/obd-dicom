a092a06b9673fae13b15efc960154274c6d56d60
Tri <tri.nguyentai@t2technology.fr>
2024-09-10 10:31:44 +0200

    Fix store multiple files and refactor tests

--- services/scu_test.go
+++ services/scu_test.go
@@ -18,9 +18,11 @@ const (
 	scp_port = 1104
 )
 
+var scp_dst = &network.Destination{Port: 1104, CalledAE: scp_aet, CallingAE: "SCU"}
+
 func TestEchoSCU(t *testing.T) {
 	assert.Error(t, NewSCU(&network.Destination{Port: scp_port}).EchoSCU(1), "Should not have C-Echo Success")
-	assert.NoError(t, NewSCU(&network.Destination{Port: 1104, CalledAE: scp_aet, CallingAE: "SCU"}).EchoSCU(1), "Should have C-Echo Success")
+	assert.NoError(t, NewSCU(scp_dst).EchoSCU(1), "Should have C-Echo Success")
 }
 
 func Test_scu_FindSCU(t *testing.T) {
@@ -92,123 +94,45 @@ func Test_scu_FindSCU(t *testing.T) {
 	}
 }
 
-func Test_scu_StoreSCU(t *testing.T) {
-	_, testSCP := StartSCP(t, 1042)
-
-	testSCP.OnAssociationRequest(func(request *network.AAssociationRQ) bool {
-		return request.GetCalledAE() == "TEST_SCP"
-	})
-
-	testSCP.OnCStoreRequest(func(request *network.AAssociationRQ, data *media.DcmObj) uint16 {
-		return dicomstatus.Success
-	})
-
-	media.InitDict()
-
-	type fields struct {
-		destination *network.Destination
-	}
+func TestStoreSCU(t *testing.T) {
 	type args struct {
 		FileNames        []string
-		timeout          int
 		transfersyntaxes []string
 	}
 	tests := []struct {
 		name    string
-		fields  fields
 		args    args
 		wantErr bool
 	}{
 		{
-			name: "Should store DICOM file",
-			fields: fields{
-				destination: &network.Destination{
-					Name:      "Test Destination",
-					CalledAE:  "TEST_SCP",
-					CallingAE: "TEST_SCU",
-					HostName:  "localhost",
-					Port:      1042,
-					IsCFind:   true,
-					IsCMove:   true,
-					IsCStore:  true,
-					IsTLS:     false,
-				},
-			},
+			name: "Should store multiples files",
 			args: args{
-				FileNames: []string{"../samples/test.dcm"},
-				timeout:   0,
+				FileNames:        []string{"../samples/test.dcm", "../samples/test2.dcm"},
+				transfersyntaxes: []string{transfersyntax.ExplicitVRLittleEndian.UID},
 			},
 			wantErr: false,
 		},
 		{
-			name: "Should store Lossless SV1 DICOM file",
-			fields: fields{
-				destination: &network.Destination{
-					Name:      "Test Destination",
-					CalledAE:  "TEST_SCP",
-					CallingAE: "TEST_SCU",
-					HostName:  "localhost",
-					Port:      1042,
-					IsCFind:   true,
-					IsCMove:   true,
-					IsCStore:  true,
-					IsTLS:     false,
-				},
-			},
+			name: "Should store lossless",
 			args: args{
-				FileNames: []string{"../samples/test-losslessSV1.dcm"},
-				timeout:   0,
-			},
-			wantErr: false,
-		},
-		{
-			name: "Should store multiples Lossless SV1 DICOM files",
-			fields: fields{
-				destination: &network.Destination{
-					Name:      "Test Destination",
-					CalledAE:  "TEST_SCP",
-					CallingAE: "TEST_SCU",
-					HostName:  "localhost",
-					Port:      1042,
-					IsCFind:   true,
-					IsCMove:   true,
-					IsCStore:  true,
-					IsTLS:     false,
-				},
-			},
-			args: args{
-				FileNames: []string{"../samples/test-losslessSV1.dcm", "../samples/MR-lossless.dcm"},
-				timeout:   0,
+				FileNames:        []string{"../samples/test-losslessSV1.dcm"},
+				transfersyntaxes: []string{transfersyntax.JPEGLosslessSV1.UID},
 			},
 			wantErr: false,
 		},
 		{
 			name: "Should transcode file to send",
-			fields: fields{
-				destination: &network.Destination{
-					Name:      "Test Destination",
-					CalledAE:  "TEST_SCP",
-					CallingAE: "TEST_SCU",
-					HostName:  "localhost",
-					Port:      1042,
-					IsCFind:   true,
-					IsCMove:   true,
-					IsCStore:  true,
-					IsTLS:     false,
-				},
-			},
 			args: args{
 				FileNames:        []string{"../samples/test-losslessSV1.dcm"},
-				timeout:          0,
-				transfersyntaxes: []string{transfersyntax.ExplicitVRLittleEndian.UID},
+				transfersyntaxes: []string{transfersyntax.ImplicitVRLittleEndian.UID},
 			},
 			wantErr: false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			d := NewSCU(tt.fields.destination)
-			if err := d.StoreSCU(tt.args.FileNames, tt.args.timeout, tt.args.transfersyntaxes...); (err != nil) != tt.wantErr {
+			d := NewSCU(scp_dst)
+			if err := d.StoreSCU(tt.args.FileNames, 0, tt.args.transfersyntaxes...); (err != nil) != tt.wantErr {
 				t.Errorf("scu.StoreSCU() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
