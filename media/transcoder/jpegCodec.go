36b8af07fc7911cd38b8f615f037ebd2b767a030
Tri <tri.nguyentai@t2technology.fr>
2024-09-16 14:29:11 +0200

    Fix tests

--- media/transcoder/jpegCodec.go
+++ media/transcoder/jpegCodec.go
@@ -9,7 +9,6 @@ import (
 
 func init() {
 	transfersyntax.RegisterCodec(transfersyntax.JPEGLosslessSV1.UID, jpegDecode, jpegEncode)
-	transfersyntax.RegisterCodec(transfersyntax.JPEGLossless.UID, jpegDecode, jpegEncode)
 	transfersyntax.RegisterCodec(transfersyntax.JPEGBaseline8Bit.UID, jpegDecode, jpegEncode)
 	transfersyntax.RegisterCodec(transfersyntax.JPEGExtended12Bit.UID, jpeg12Decode, jpeg12Encode)
 }
